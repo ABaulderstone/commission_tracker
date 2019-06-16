55fb9ef78262419c2d2e7fa03fa03cd03bb1d4dc
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-06-16 20:27:27 +1000

    added post and get routes to shifts

--- controllers/shift_controller.js
+++ controllers/shift_controller.js
@@ -4,3 +4,11 @@ async function index(req, res) {
     let shifts = await ShiftModel.find();
     res.render("shifts/index", {shifts});
 }
+
+async function create(req, res) {
+    let shift = await ShiftModel.create()
+    .catch(err => res.status(500).send(err));
+
+    res.redirect("/shifts");
+
+}
--- routes/shift_routes.js
+++ routes/shift_routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const ShiftController = require("./../controllers/shift_controller");
+const SaleController = require("./../controllers/sale_controller")
 
+router.get("/", ShiftController.index);
 
+router.post("/", ShiftController.create);
 
 module.exports = router;
